fa130e543cb434f799d85292f651904917b39504
Ashique Sherief <ashique.sherief@gmail.com>
2023-11-19 20:20:51 +0530

    poster css prop change

--- src/Compounents/Poster.jsx
+++ src/Compounents/Poster.jsx
@@ -11,17 +11,19 @@ const Poster = () => {
   return (
     <div>
       <div className="poster md:flex justify-evenly bg-indigo-100 py-6 md:py-12">
-        <div className="md:flex md:w-1/3 md:flex-wrap mt-6 md:mt-12 items-start">
-          <h1 className="text-4xl md:text-5xl mb-2 font-bold text-white text-left">
-            Premium Quality
-            <br />
-            Mobile Batteries
-            <br />
-          </h1>
-          <h3 className="text-large md:text-large mb-2 text-white text-left">
-            We are India’s biggest lithium-ion mobile battery brand. We got a
-            battery for all your needs.
-          </h3>
+        <div className="md:flex md:w-1/3 md:pb-20 items-center md:flex-wrap mt-6 md:mt-12">
+          <div>
+            <h1 className="text-4xl md:text-5xl mb-2 font-bold text-white text-left">
+              Premium Quality
+              <br />
+              Mobile Batteries
+              <br />
+            </h1>
+            <h3 className="text-large md:text-large mb-2 text-white text-left">
+              We are India’s biggest lithium-ion mobile battery brand. We got a
+              battery for all your needs.
+            </h3>
+          </div>
         </div>
         <div className="md:flex md:w-1/3 md:flex-wrap mt-6 md:mt-12 items-start">
           <Box
